11ac18d76aa51e6721bd0a88ad5d7ad370d3fa6e
wangjunling <yogoloth@gmail.com>
2022-07-11 11:23:51 +0800

    修改：     haproxy_trace.c 	新文件：   sample/pid

--- haproxy_trace.c
+++ haproxy_trace.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
-int main(){
+#include <fcntl.h>
 
+
+typedef enum {
+   from,
+   to
+} dirt;
+
+char*
+fd_to_ports(pid,fd,proc_file,sock_file,d)
+    pid_t pid;
+    int fd;
+    char* proc_file;
+    char* sock_file;
+    dirt d;
+{
+    return NULL;
+}
+
+int 
+main(argc,argv)
+    int argc;
+    char *argv[];
+{
+    char* ports_str=fd_to_ports(3321,609,"sample/proc_file","sample/socket_file",from);
+    printf("%s\n",ports_str);
+    return 0;
 }
