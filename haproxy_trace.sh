11d92c5f343bb976042ad49e4fdc785851ba65f3
wangjunling <yogoloth@gmail.com>
2022-08-16 16:03:06 +0800

    修改：     haproxy_trace.sh

--- haproxy_trace.sh
+++ haproxy_trace.sh
@@ -2,6 +2,8 @@
 
 #pid=3321
 
+haproxy_socket=${1:-/var/lib/haproxy/stats}
+
 pid=`ps ax| grep haproxy| grep -v grep| awk 'NR==1{print $1}'`
 
 proc_file=/proc/net/tcp
@@ -12,7 +14,7 @@ tmp_sess=/tmp/haproxy_sess
 cp $proc_file $tmp_proc_file
 ls -lh /proc/$pid/fd/ |grep socket: | awk '{print $9" "$11}' >$tmp_socket_file
 #echo show sess| socat /var/lib/haproxy/stats stdio | grep -v fd=-1 | awk '{print $3" "$4" "$5" "$16" "$17 }'| sed 's/s0=\[.*fd=\([0-9]\+\).*\].*s1=.*\[.*fd=\([0-9]\+\).*\]/\1 \2/g' >$tmp_sess
-./get_sess /var/lib/haproxy/stats stdio | grep -v fd=-1 | awk '{print $3" "$4" "$5" "$16" "$17 }'| sed 's/s0=\[.*fd=\([0-9]\+\).*\].*s1=.*\[.*fd=\([0-9]\+\).*\]/\1 \2/g' >$tmp_sess
+./get_sess ${haproxy_socket} stdio | grep -v fd=-1 | awk '{print $3" "$4" "$5" "$16" "$17 }'| sed 's/s0=\[.*fd=\([0-9]\+\).*\].*s1=.*\[.*fd=\([0-9]\+\).*\]/\1 \2/g' >$tmp_sess
 
 
 ./haproxy_trace $tmp_sess $tmp_proc_file $tmp_socket_file
